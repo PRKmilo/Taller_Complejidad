fc9f443dbe97b4ecb69401fb2cee4dca97e0ff78
jpizar@unbosque.edu.co <jpizarodrugiez@gmail.com>
2022-09-01 00:07:22 -0500

    cambios con tree sort y redixsort

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,25 +1,55 @@
 package co.edu.unbosque.controller;
 
-import co.edu.unbosque.model.Numeros;
+
 import co.edu.unbosque.model.QuickSort;
+import co.edu.unbosque.model.Radixsort;
+import co.edu.unbosque.model.TreeSort;
 import co.edu.unbosque.view.VistaVentanas;
 
 public class Controller {
 	
 	private VistaVentanas vista;
 	private QuickSort quick;
+	private Radixsort radix;
+	private TreeSort tree;
 
 	
 	public Controller() {
 		vista = new VistaVentanas();
-
-		quick=new QuickSort();
-		funcionar();
+		tree=new TreeSort();
+		//quick=new QuickSort();
+		radix=new Radixsort();
+		//funcionar();
+		//int arr[] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+        //int n = arr.length;
+        //System.out.println("numero aleatorio "+ (int) (Math.random() * 900) + 1);
+        //int arr2[]=new int[60000000];
+        //for(int i=0;i<60000000;i++) {
+        	//arr2[i]=(int) (Math.random() * 900) + 1;
+        //}
+        // Function Call 
+        //System.out.println("antes de funcion");
+        //radix.radixsort(arr2, 60000000);
+        //radix.print(arr2, 60000000);
+        //System.out.println("despues de la funcion");
+		int arr3[]=new int[60000000];
+		for(int i=0;i<arr3.length;i++) {
+			arr3[i]=(int) (Math.random() * 900) + 1;
+		}
+		long startTime = System.currentTimeMillis();
+       for(int i=0;i<60000000;i++) {
+    	   tree.insertar(arr3[i]," ");
+       }
+       tree.recorrer(tree.raiz);
+        tree.regreso();
+        long endTime = System.currentTimeMillis() - startTime;
+        System.out.println("tiempo de ejecucion "+endTime);
+        	
 	}
 	
 	public void funcionar() {
 
-		quick.principal();
+		
 // System.out.println(num.generarAleatorio());
  
 	System.out.println("----------------------------");
--- src/co/edu/unbosque/model/TreeSort.java
+++ src/co/edu/unbosque/model/TreeSort.java
@@ -1,5 +1,105 @@
 package co.edu.unbosque.model;
 
+import java.util.ArrayList;
+
 public class TreeSort {
+	public static Nodo raiz;
+	public static ArrayList<Integer> retorno;
+	
+	public TreeSort() {
+		raiz=null;
+		retorno=new ArrayList<>();
+	}
+	
+	
+
+
+
+	public void insertar(int i,Object numero) {
+		Nodo n=new Nodo(i);
+		System.out.println("este es el indice "+i);
+		n.contenido = numero;
+		if(raiz == null) {
+			
+			raiz = n;
+		}else {
+			Nodo aux = raiz;
+			
+			while(aux != null) {
+				n.padre=aux;
+				if(n.llave >= aux.llave) {
+					
+					aux=aux.derecha;
+				}else {
+					
+					aux=aux.izquierda;
+				}
+			}
+			if(n.llave < n.padre.llave) {
+				
+				n.padre.izquierda = n;
+			}else {
+				n.padre.derecha=n;
+			}
+		}
+	}
+	
+	public void recorrer(Nodo n) {
+		if(n != null) {
+			recorrer(n.izquierda);
+			
+			recorrer(n.derecha);
+			if(n.padre != null) {
+				retorno.add(n.padre.llave);
+				
+			}
+		}
+	}
+	
+	public Nodo encontrarnodo(int value,Nodo raiz) {
+		Nodo aux=raiz;
+		while(aux.llave != value) {
+			if(value < aux.llave) {
+				aux=aux.izquierda;
+			}else {
+				aux=aux.derecha;
+			}if(aux==null) {
+				return null;
+			}
+		}
+		return aux;
+	}
+	
+	public Nodo ancestro(Nodo raiz,Nodo x,Nodo y) {
+		if(raiz == null) {
+			return null;
+		}
+		if(raiz == x || raiz == y) {
+			return raiz;
+		}
+		Nodo left = ancestro(raiz.izquierda,x,y);
+		
+		Nodo right = ancestro(raiz.derecha,x,y);
+		
+		if (left != null && right != null) {
+			return raiz;
+		}
+		
+		if (left != null) {
+			return left;
+		}
+		
+		if (right != null) {
+			return right;
+		}
+		
+		return null;
+	}
+	
+	public void regreso() {
+		
+		System.out.println(retorno);
+			
+	}
 
 }
